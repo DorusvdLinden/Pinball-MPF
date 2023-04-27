d839dafab6a45b0c295af68a1c30124af82305aa
Dorus van der Linden <46962795+DorusvdLinden@users.noreply.github.com>
2023-04-27 12:17:07 +0200

    servo

--- mpfv1/pinballrun.bat
+++ mpfv1/pinballrun.bat
@@ -1,3 +1,3 @@
-cd C:\pinball\Pinball-MPF\mpfv1
+cd C:\Users\dorus\OneDrive\Documents\Pinball\Github\Pinball-MPF\mpfv1
 start C:\Users\dorus\AppData\Local\Programs\Python\Python38\python.exe -m mpf monitor
 C:\Users\dorus\AppData\Local\Programs\Python\Python38\python.exe -m mpf both -X
